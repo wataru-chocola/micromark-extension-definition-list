d787846378a9e3a2f3272c4ca245cd29023a88bb
Wataru Watanabe <wataru.chocola@gmail.com>
2021-09-22 13:34:55 +0900

    add file extension in import for nodejs and browser ESM compatibility

--- dev/index.ts
+++ dev/index.ts
@@ -1,3 +1,3 @@
-export { defList } from './lib/syntax';
-export { defListHtml } from './lib/html';
-export { tokenTypes as types } from './lib/types';
+export { defList } from './lib/syntax.js';
+export { defListHtml } from './lib/html.js';
+export { tokenTypes as types } from './lib/types.js';
